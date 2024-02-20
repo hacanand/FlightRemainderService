807d31b2987475e1ef34cdd77eabd554856e5069
anand <anandpromax@gmail.com>
2024-02-20 18:04:43 +0530

    Add .gitignore, serverConfig.js, index.js, and package.json

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { PORT } = require("./config/serverConfig");
+const bodyParser = require("body-parser");
+const app = express();
+const setupAndStartServer = async () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port  ${PORT}`);
+  });
+};
+setupAndStartServer();
